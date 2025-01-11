d26d45075cbe38529f619ea75eeb393d2a6290d5
juancolchete <juanudkp@gmail.com>
2025-01-11 23:52:05 +0000

    feat: basic image edition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,82 @@
 "use client"
-import '@pqina/pintura/pintura.css';
-import './App.css';
-import { PinturaEditor } from '@pqina/react-pintura';
+import React, { useState } from "react";
+
+// react-pintura
+import { PinturaEditor } from "@pqina/react-pintura";
+
+// pintura
+import { pintura } from "@pqina/pintura/pintura.module.css";
+import { index as pinturaTheme } from "./index.module.css";
+
 import {
-    getEditorDefaults,
-    createMarkupEditorToolStyle,
-    createMarkupEditorToolStyles,
-} from '@pqina/pintura';
+  // editor
+  createDefaultImageReader,
+  createDefaultImageWriter,
+  createDefaultShapePreprocessor,
 
-const editorDefaults = getEditorDefaults();
+  // plugins
+  setPlugins,
+  plugin_crop,
+  plugin_finetune,
+  plugin_finetune_defaults,
+  plugin_filter,
+  plugin_filter_defaults,
+  plugin_annotate,
+  markup_editor_defaults,
+} from "@pqina/pintura";
 
-function Home() {
-    return (
-        <div className="App">
-            <PinturaEditor
-                {...editorDefaults}
-                src={'image.jpeg'}
-                markupEditorToolStyles={createMarkupEditorToolStyles({
-                    text: createMarkupEditorToolStyle('text', {
-                        // Set default text shape background to transparent
-                        backgroundColor: [0, 0, 0, 0],
+import {
+  LocaleCore,
+  LocaleCrop,
+  LocaleFinetune,
+  LocaleFilter,
+  LocaleAnnotate,
+  LocaleMarkupEditor,
+} from "@pqina/pintura/locale/en_GB";
 
-                        // Set default text shape outline to 0 width and white
-                        textOutline: ['0%', [1, 1, 1]],
+setPlugins(plugin_crop, plugin_finetune, plugin_filter, plugin_annotate);
 
-                        // Set default text shadow, shadow will not be drawn if x, y, and blur are 0.
-                        textShadow: ['0%', '0%', '0%', [0, 0, 0, 0.5]],
+const editorDefaults = {
+  utils: ["crop", "finetune", "filter", "annotate"],
+  imageReader: createDefaultImageReader(),
+  imageWriter: createDefaultImageWriter(),
+  shapePreprocessor: createDefaultShapePreprocessor(),
+  ...plugin_finetune_defaults,
+  ...plugin_filter_defaults,
+  ...markup_editor_defaults,
+  locale: {
+    ...LocaleCore,
+    ...LocaleCrop,
+    ...LocaleFinetune,
+    ...LocaleFilter,
+    ...LocaleAnnotate,
+    ...LocaleMarkupEditor,
+  },
+};
 
-                        // Allow newlines in inline text
-                        disableNewline: false,
+export default function Example() {
+  // inline
+  const [result, setResult] = useState("");
 
-                        // Align to left by default, this triggers always showing the text align control
-                        textAlign: 'left',
+  return (
+    <div>
+      <h2>Example</h2>
 
-                        // Enable text formatting
-                        format: 'html',
-                    }),
-                })}
-            />
-        </div>
-    );
+      <div style={{ height: "70vh" }}>
+        <PinturaEditor
+          {...editorDefaults}
+          className={`${pintura} ${pinturaTheme}`}
+          src={"./image.jpeg"}
+          onLoad={(res) => console.log("load image", res)}
+          onProcess={({ dest }) => setResult(URL.createObjectURL(dest))}
+        />
+      </div>
+
+      {!!result.length && (
+        <p>
+          <img src={result} alt="" />
+        </p>
+      )}
+    </div>
+  );
 }
-
-export default Home;
