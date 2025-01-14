1adbbd1350980c42457df600cf692746fa10fe49
juancolchete <juanudkp@gmail.com>
2025-01-14 15:18:57 +0000

    feat: mount thum-gen thumb maker

--- genThumb.sh
+++ genThumb.sh
@@ -1,2 +1,4 @@
 magick -size 1280x720 -fill cyan -background none -pointsize 24 label:Hat   -gravity North outfile.jpeg
-magick composite  image.jpeg outfile.jpeg   compose_under.png
+magick image.jpeg -resize 870x510!  image.jpeg
+magick composite -gravity southeast image.jpeg outfile.jpeg   compose_under.png
+magick composite -gravity southwest man.png compose_under.png   compose_under.png
