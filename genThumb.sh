1a1846e95b4dd727960f2e925f7e20fc02449344
juancolchete <juanudkp@gmail.com>
2025-01-15 14:30:35 +0000

    fix: use outfile as png

--- genThumb.sh
+++ genThumb.sh
@@ -1,6 +1,6 @@
-magick -size 1280x720 xc:"rgba(27,33,44,1)" outfile.jpeg
+magick -size 1280x720 xc:"rgba(27,33,44,1)" outfile.png
 magick image.jpeg -resize 870x510!  image.png
-magick composite -gravity southeast image.png outfile.jpeg   compose_under.png
+magick composite -gravity southeast image.png outfile.png   compose_under.png
 magick composite -gravity southwest man.png compose_under.png   compose_under.png
 magick -pointsize 60 -gravity center -fill white -background "rgba(27,33,44,1)" -size 1280x210 -font 'Open-Sans-ExtraBold' label:'Adicionar imagem sudoeste #7' text.png
 magick composite -gravity north text.png compose_under.png   compose_under.png
