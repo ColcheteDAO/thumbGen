3f3d8557aa8d49ceb10598dfa18c8717ee1a6ace
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2025-01-20 02:23:54 +0000

    ci: just convert first outile

--- genThumb.sh
+++ genThumb.sh
@@ -1,6 +1,6 @@
-magick -size 1280x720 xc:"rgba(27,33,44,1)" outfile.png
-magick "$2".png -resize 870x510!  image.png
-magick composite -gravity southeast image.png outfile.png   compose_under.png
-magick composite -gravity southwest man.png compose_under.png   compose_under.png
-magick -pointsize 60 -gravity center -fill white -background "rgba(27,33,44,1)" -size 1280x210 -font 'Open-Sans-ExtraBold' label:"$1" text.png
-magick composite -gravity north text.png compose_under.png   compose_under.png
+convert -size 1280x720 xc:"rgba(27,33,44,1)" outfile.png
+#convert "$2".png -resize 870x510!  image.png
+#convert composite -gravity southeast image.png outfile.png   compose_under.png
+#convert composite -gravity southwest man.png compose_under.png   compose_under.png
+#convert -pointsize 60 -gravity center -fill white -background "rgba(27,33,44,1)" -size 1280x210 -font 'Open-Sans-ExtraBold' label:"$1" text.png
+#convert composite -gravity north text.png compose_under.png   compose_under.png
