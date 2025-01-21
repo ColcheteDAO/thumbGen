5d7f210ae32a04d9cdd8b53d59e39290ea25ee2c
juancolchete <juanudkp@gmail.com>
2025-01-21 14:46:14 +0000

    fix: use convert in all remain lines

--- genThumb.sh
+++ genThumb.sh
@@ -1,6 +1,6 @@
 convert -size 1280x720 xc:"rgba(27,33,44,1)" outfile.png
 convert "$2".png -resize 870x510!  image.png
 convert -gravity southeast outfile.png image.png  -composite compose_under.png
-# magick composite -gravity southwest man.png compose_under.png   compose_under.png
-# magick -pointsize 60 -gravity center -fill white -background "rgba(27,33,44,1)" -size 1280x210 -font 'Open-Sans-ExtraBold' label:"$1" text.png
-# magick composite -gravity north text.png compose_under.png   compose_under.png
+convert -gravity southwest compose_under.png man.png -composite   compose_under.png
+convert -pointsize 60 -gravity center -fill white -background "rgba(27,33,44,1)" -size 1280x210 -font 'Open-Sans-ExtraBold' label:"$1" text.png
+convert -gravity north compose_under.png text.png -composite  compose_under.png
