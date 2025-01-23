b924ab5dbe54cd101388716e738d7ec14cc57c48
juancolchete <juanudkp@gmail.com>
2025-01-23 12:58:43 +0000

    feat: auto update video thumb

--- processVideos.sh
+++ processVideos.sh
@@ -8,16 +8,20 @@ while IFS= read -r line; do
     lastChar=$((${#line}+2))
     folder=$(echo "$line" | cut -c 3-$lastChar)
     index=0
-  elif [ $headingCounter = 2 ]; then
+  elif [ $headingCounter = 3 ]; then
     index=$((${index}+1))
     lastChar=$((${#line}+2))
     title=$(echo "$line" | cut -c 4-$lastChar)
     bash genThumb.sh "$title" "$folder" 
     mkdir -p "out/$folder"
-    path="out/$folder/$folder$index".png
+    path="out/$folder/$folder$index.png"
     mv compose_under.png $path
   elif [ $videoCount = 1 ]; then
     lastChar=$((${#line}-1))
-    echo "$line" | cut -c 26-$lastChar
+    videoId=$(echo "$line" | cut -c 26-$lastChar)
+    curl --request POST -v "https://www.googleapis.com/upload/youtube/v3/thumbnails/set?videoId=${videoId}&uploadType=media" \
+    --header "Authorization: Bearer $1" \
+    --header 'Content-Type: image/jpeg' \
+    --data-binary "@$path"
   fi
 done < videos.md
