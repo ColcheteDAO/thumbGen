2c94a05e22964d760b6bfb39fef4c2baa9d221a3
juancolchete <juanudkp@gmail.com>
2025-01-24 13:47:07 +0000

    fix!: video id with dash

--- processVideos.sh
+++ processVideos.sh
@@ -20,7 +20,7 @@ while IFS= read -r line; do
   elif [ $videoCount = 1 ]; then
     lastChar=$((${#line}-1))
     videoId=$(echo "$line" | cut -c 26-$lastChar)
-    curl --request POST -v "https://www.googleapis.com/upload/youtube/v3/thumbnails/set?videoId=${videoId}&uploadType=media" \
+    curl --request POST -v "https://www.googleapis.com/upload/youtube/v3/thumbnails/set?videoId=$videoId&uploadType=media" \
     --header "Authorization: Bearer $ACCESS_TOKEN" \
     --header 'Content-Type: image/jpeg' \
     --data-binary "@$path"
