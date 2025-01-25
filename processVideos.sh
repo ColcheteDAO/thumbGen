c1d8ec1e34260d6ecf9458c4664233a5187ae56e
juancolchete <juanudkp@gmail.com>
2025-01-25 21:51:53 +0000

    debug: show description and category

--- processVideos.sh
+++ processVideos.sh
@@ -23,7 +23,8 @@ while IFS= read -r line; do
     listReq=$(curl "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=$videoId" \
     --header "Authorization: Bearer $ACCESS_TOKEN" \
     --header "Accept: application/json")
-    echo $listReq | jq .items[0].snippet.description
+    echo $listReq | jq .items[0].snippet.categoryId
+    echo $listReq | jq .items[0].snippet.title
     descriptionLen=$(echo $listReq | jq .items[0].snippet.description | wc -m)
     # echo '{"id":"qr5LTIogHxQ","snippet":{"description":"abc","title":"'$titleVideo'","categoryId":"'$categoryId'"}}'
     if [ $descriptionLen -lt 10 ]; then
