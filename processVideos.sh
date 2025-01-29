2527363e9f64be57b68f913eb7ba80bb5f9ff840
juancolchete <juanudkp@gmail.com>
2025-01-29 14:37:32 +0000

    debug: list video

--- processVideos.sh
+++ processVideos.sh
@@ -52,7 +52,7 @@ while IFS= read -r line; do
     if [ $descriptionLen -lt 10 ] || [ "$4" = "Y" ]; then
       echo $4
       echo $descriptionLen
-      echo $listReq | jq .items[0].snippet.description
+      echo $listReq | jq .items[0]
       # curl --request POST -v "https://www.googleapis.com/upload/youtube/v3/thumbnails/set?videoId=$videoId&uploadType=media" \
       # --header "Authorization: Bearer $ACCESS_TOKEN" \
       # --header "Content-Type: image/jpeg" \
