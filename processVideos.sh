50b00ae9dbb0347d2b2b928e1e5c83ddda4f6a42
juancolchete <juanudkp@gmail.com>
2025-01-30 11:45:21 +0000

    feat: update by index

--- processVideos.sh
+++ processVideos.sh
@@ -8,11 +8,13 @@ fillDescription=false
 list1=''
 list2=''
 declare -A playlists
-ACCESS_TOKEN=$(curl  --location --request POST "https://oauth2.googleapis.com/token?client_secret=$1&grant_type=refresh_token&refresh_token=$2&client_id=$3" | jq .access_token | tr -d '"')
+startUpdateIndex=0
+# ACCESS_TOKEN=$(curl  --location --request POST "https://oauth2.googleapis.com/token?client_secret=$1&grant_type=refresh_token&refresh_token=$2&client_id=$3" | jq .access_token | tr -d '"')
 while IFS= read -r line; do
   headingCounter=$(echo $line | grep -o '#' | wc -l)
   videoCount=$(echo $line | grep -o '\[video\]' | wc -l)
   playlistCount=$(echo $line | grep -o '\[playlist\]' | wc -l)
+  startUpdateIndexCount=$(echo $line | grep -o '\*\*index\*\*: ' | wc -l)
   if [ $headingCounter = 1 ]; then
     lastChar=$((${#line}+2))
     folder=$(echo "$line" | cut -c 3-$lastChar)
@@ -23,7 +25,7 @@ while IFS= read -r line; do
     fillDescription=false
     description=$(echo $line)
   elif [ $playlistCount = 1 ]; then
-    lastChar=$((${#line}-1))
+    lastChar=$((${#line}-3))
     playlistId=$(echo "$line" | cut -c 51-$lastChar)
     if [ $playlistIndex = 0 ]; then
       list1=$(echo $playlistId) 
@@ -39,8 +41,11 @@ while IFS= read -r line; do
     mkdir -p "out/$folder"
     path="out/$folder/$folder$index.png"
     mv compose_under.png $path
+  elif [ $startUpdateIndex = 1]; then
+    lastChar=$((${#line}-3))
+    startUpdateIndex=$(echo "$line" | cut -c 11-$lastChar)
   elif [ $videoCount = 1 ]; then
-    lastChar=$((${#line}-1))
+    lastChar=$((${#line}-3))
     videoId=$(echo "$line" | cut -c 26-$lastChar)
     listReq=$(curl "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=$videoId" \
     --header "Authorization: Bearer $ACCESS_TOKEN" \
@@ -49,7 +54,7 @@ while IFS= read -r line; do
     titleVideo=$(echo $listReq | jq -r .items[0].snippet.title)
     echo $titleVideo
     descriptionLen=$(echo $listReq | jq .items[0].snippet.description | wc -m)
-    if [[ ! -z "$description" ]] && [ $descriptionLen -lt 10 ] || [ "$4" = "Y" ]; then
+    if [[ ! -z "$description" ]] && [ $descriptionLen -lt 10 ] || [ "$4" = "Y" ] || [$index -ge $startUpdateIndex]; then
       curl --request POST -v "https://www.googleapis.com/upload/youtube/v3/thumbnails/set?videoId=$videoId&uploadType=media" \
       --header "Authorization: Bearer $ACCESS_TOKEN" \
       --header "Content-Type: image/jpeg" \
--- videos.md
+++ videos.md
@@ -1,7 +1,8 @@
 # thumbGen
-Repo:\nhttps://github.com/ColcheteDAO/thumbGen
-[playlist](https://youtu.be/watch?v=playlist&list=PLbWtSW17vSe6diSfeAdVqzF7aLwRw8UeA)
-[playlist](https://youtu.be/watch?v=playlist&list=PLbWtSW17vSe6-0d3pFM7_W2eQjUwWqHe5)
+Repo:\nhttps://github.com/ColcheteDAO/thumbGen  
+[playlist](https://youtu.be/watch?v=playlist&list=PLbWtSW17vSe6diSfeAdVqzF7aLwRw8UeA)  
+[playlist](https://youtu.be/watch?v=playlist&list=PLbWtSW17vSe6-0d3pFM7_W2eQjUwWqHe5)  
+**index**: 22  
 ## Configuração inicial #1
 [video](https://youtu.be/yXydAGckYZ8)
 ## Exibição da imagem #2
