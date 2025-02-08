f79a813d487a1ad337355cdb147019e5c34999c0
juancolchete <juanudkp@gmail.com>
2025-02-08 13:22:10 +0000

    debug: show kind

--- processVideos.sh
+++ processVideos.sh
@@ -95,7 +95,7 @@ mountVideosMeta(){
   videosSearch=$(sendGetRequest "$urlBaseAPI/youtube/v3/search?part=snippet&forMine=true&maxResults=50&order=date&q=$1&type=video")
   while read videoSearchItem
   do
-    echo "$videoSearchItem"
+    echo "$videoSearchItem" | jq -c '.kind'
   done < <(echo "$videosSearch" | jq -c '.items[]')
 
 }
