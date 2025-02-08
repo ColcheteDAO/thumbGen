9a53d9a3730c05e1e685c728cb1fd77d833853b8
juancolchete <juanudkp@gmail.com>
2025-02-08 13:23:17 +0000

    feat: pass title and description

--- processVideos.sh
+++ processVideos.sh
@@ -95,7 +95,8 @@ mountVideosMeta(){
   videosSearch=$(sendGetRequest "$urlBaseAPI/youtube/v3/search?part=snippet&forMine=true&maxResults=50&order=date&q=$1&type=video")
   while read videoSearchItem
   do
-    echo "$videoSearchItem" | jq -c '.kind'
+    echo "$videoSearchItem" | jq -c '.title'
+    echo "$videoSearchItem" | jq -c '.description'
   done < <(echo "$videosSearch" | jq -c '.items[]')
 
 }
