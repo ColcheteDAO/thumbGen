3830011d2314014661326d20a17e5586d824459b
juancolchete <juanudkp@gmail.com>
2025-02-11 15:39:00 +0000

    debug: echo series number

--- processVideos.sh
+++ processVideos.sh
@@ -102,7 +102,10 @@ mountVideosMeta(){
     titleIndexRaw=$(echo "$videoTitleRaw"| grep -o -b $folder )
     titleIndexRawLen=${#titleIndexRaw}
     titleIndex=$(echo $titleIndexRaw | cut -c 1-$(expr $titleIndexRawLen - $folderStrLen - 1))
-    echo $titleIndexRaw | cut -c $titleIndex-$videoTitleRawLen
+    echo "aaaa"
+      seriesNumber=$(echo $titleIndexRaw | cut -c $titleIndex-$videoTitleRawLen)
+      echo $seriesNumber
+    echo "aaaa" 
     echo "## ${videoTitleRaw/$folder /"#"}"
     videoIdAPI=$(echo "$videoSearchItem" | jq -r '.id.videoId')
     echo "[video](https://youtu.be/$videoIdAPI)"
