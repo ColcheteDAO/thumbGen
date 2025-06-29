5d1e4687b0f7308a0fbb0a6360149ae4738d09f9
juancolchete <juanudkp@gmail.com>
2025-06-29 20:01:15 +0000

    debug: log video search

--- processVideos.sh
+++ processVideos.sh
@@ -129,6 +129,9 @@ mountVideosMeta(){
   saveVideosMeta(){
     videoSeriesQuery=$(echo -n "$1" | jq -sRr @uri)
     videosSearch=$(sendGetRequest "$urlBaseAPI/youtube/v3/search?part=snippet&forMine=true&maxResults=50&order=date&q=$videoSeriesQuery&type=video&pageToken=$2")
+    echo "serac"
+    echo "$videosSearch"
+    echo "serac"
     if [[ "$videosSearch" == "error" ]]; then
       errorMSG=$videosSearch
       echo "$videosSearch $funName ${errors[0]}"
