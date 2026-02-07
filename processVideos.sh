ad916e7ee734a170ec72a15e48e10ab0890ed388
juancolchete <juanudkp@gmail.com>
2026-02-07 09:13:35 -0300

    ci: re test custom props

--- processVideos.sh
+++ processVideos.sh
@@ -127,7 +127,7 @@ mountVideoCustomProps(){
     [[ $isCustomProps == false ]] && {
       videoTitle=$(sed "$lineIndex!d" "out/titles/$1.md" )
       defaultCustomVideoProp=$(jq -n --arg title "$videoTitle" --argjson config "$(cat "config/$folder.json")" \
-      '{title: $title, description: $config.description, tags: $config.tags, custom: false, updatedThumb: false}' -c)
+      '{title: $title, description: $config.description, tags: $config.tags, custom: true, updatedThumb: false}' -c)
       echo "$(jq '.['$i'] = '"$defaultCustomVideoProp"'' "out/custom/$1.json")" > "out/custom/$1.json" 
       lineIndex=$(( lineIndex + 2 ))
     }
